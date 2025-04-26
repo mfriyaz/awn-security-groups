657c8a6b4d962579da6b4b52a121c17ebd5b4ce8
Riyaz <mfriyaz@gmail.com>
2025-04-26 22:02:31 +0800

    network interface update_3

---
+++ awnuat2021/main.tf
@@ -0,0 +1,23 @@
+# Module for creating the SG
+module "rdp_mssql_sg" {
+  source      = "./modules/security_group"
+  name        = "rdp-mssql-access"
+  description = "Allow RDP and MSSQL access"
+  vpc_id      = "vpc-0a042b1c409605c9c"
+  allowed_cidrs = ["192.50.10.0/24"]
+  tags = {
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+  }
+}
+
+# Fetch the EC2 instance by ID
+data "aws_instance" "target" {
+  instance_id = "i-0fc28ab2fb7aff067"  # Replace with your actual instance ID
+}
+
+# Attach the SG to the instance's primary network interface
+resource "aws_network_interface_sg_attachment" "attach_sg" {
+  security_group_id    = module.rdp_mssql_sg.security_group_id
+  network_interface_id = "eni-05c647588dee64f24"
+}
