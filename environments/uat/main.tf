28f6712748abcbe2e7728993d5b147353a9813fe
Riyaz <mfriyaz@gmail.com>
2025-04-26 10:41:59 +0800

    aws region update

---
+++ environments/uat/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "app-southeast-1"
+}
+
+# Module for creating the SG
+module "rdp_mssql_sg" {
+  source      = "../../modules/security_group"
+  name        = "rdp-mssql-access"
+  description = "Allow RDP and MSSQL access"
+  vpc_id      = "vpc-xxxxxxxx"
+  allowed_cidrs = ["192.168.1.0/24"]
+  tags = {
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+  }
+}
+
+# Fetch the EC2 instance by ID
+data "aws_instance" "target" {
+  instance_id = "i-0abcdef1234567890"  # Replace with your actual instance ID
+}
+
+# Attach the SG to the instance's primary network interface
+resource "aws_network_interface_sg_attachment" "attach_sg" {
+  security_group_id    = module.rdp_mssql_sg.security_group_id
+  network_interface_id = data.aws_instance.target.primary_network_interface_id
+}
