ab404f61cea1df5a6ad6b9ed1171515668f56093
Riyaz <mfriyaz@gmail.com>
2025-04-26 20:01:54 +0800

    instance id update

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "rdp_mssql_sg" {
 
 # Fetch the EC2 instance by ID
 data "aws_instance" "target" {
-  instance_id = "i-0abcdef1234567890"  # Replace with your actual instance ID
+  instance_id = "i-0c4058e53e9938e97"  # Replace with your actual instance ID
 }
 
 # Attach the SG to the instance's primary network interface
